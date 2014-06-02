d96129e0e1c456e29d65c144113fb3e33d61efe9
Aaron Scherer <aequasi@gmail.com>
2014-06-01 23:59:08 -0700

    Changing markdown editor to codemirror

--- src/Bundle/CoreBundle/Resources/public/js/main.js
+++ src/Bundle/CoreBundle/Resources/public/js/main.js
@@ -1,8 +1,1 @@
-$(function() {
-    $('nav ul').jarvismenu({
-        accordion : true,
-        speed : 235,
-        closedSign : '<em class="fa fa-expand-o"></em>',
-        openedSign : '<em class="fa fa-collapse-o"></em>'
-    });
-});
+hljs.initHighlightingOnLoad();
